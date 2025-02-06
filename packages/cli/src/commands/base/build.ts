e47faf283958afdc2c2ae1d45a29e9ccc7b80490
Hkaiyue <1083683700@qq.com>
2025-02-06 22:12:38 +0800

    丰富命令与增量构建

--- packages/cli/src/commands/base/build.ts
+++ packages/cli/src/commands/base/build.ts
@@ -1,11 +1,19 @@
 import { Command } from "commander";
 import { logger } from "../../utils/logger";
-
+import { spawn } from "node:child_process";
 export function build(program: Command) {
   return program
     .createCommand("build")
     .description("build projects")
     .action((...args) => {
       logger.log("构建项目", args);
+      const command = args[0] === "npm" ? "npm run build" : "pnpm build";
+      const child = spawn(command, {
+        stdio: "inherit", // 将子进程的输出传递给父进程
+        shell: true, // 使用shell执行命令
+      });
+      child.on("close", (code) => {
+        logger.log(`子进程退出，退出码: ${code}`);
+      });
     });
 }
--- packages/cli/src/commands/base/serve.ts
+++ packages/cli/src/commands/base/serve.ts
@@ -1,11 +1,21 @@
 import { logger } from "../../utils/logger";
 import { Command } from "commander";
-
+import { spawn } from "node:child_process";
 export function serve(program: Command) {
   return program
     .createCommand("serve")
     .description("serve projects")
     .action((...args) => {
+      //执行项目的启动命令
+      //npm run dev / pnpm dev
       logger.log("启动项目", args);
+      const command = args[0] === "npm" ? "npm run dev" : "pnpm dev";
+      const child = spawn(command, {
+        stdio: "inherit", // 将子进程的输出传递给父进程
+        shell: true, // 使用shell执行命令
+      });
+      child.on("close", (code) => {
+        logger.log(`子进程退出，退出码: ${code}`);
+      });
     });
 }
